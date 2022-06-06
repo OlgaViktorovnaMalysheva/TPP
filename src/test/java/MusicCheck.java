8cbfbc89252936bafc40a9de5a6c81fceda1c889
OlgaViktorovnaMalysheva <olgaviktormal@gmail.com>
2022-06-06 15:59:59 +0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/MusicCheck.java
+++ src/test/java/MusicCheck.java
@@ -8,17 +8,18 @@ import static org.openqa.selenium.By.xpath;
 
 public class MusicCheck extends MusicStart {
     private By AllMusic = By.xpath("//*[@id=\"hook_Block_MusicToolbarButton\"]//*[contains(@class,\"toolbar_nav_i_ic\")]");
-    private By Song = By.xpath("//*[@class=\"duration\"]//*[contains(@name,\"info\")]");
+    private By Song = By.xpath("//*[contains(@data-l,\"t,top_tracks_list\")]"
+            + "/slot/top-tracks-list/wm-tracks-list/main/wm-track[position() <= 1]");
 
     @Test
     public void musicCheck() {
         driver.findElement(AllMusic).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
          if (driver.findElement(Song).getText().contains("03:31")) {
-            System.out.println("песня совпала");
+            System.out.println("song wright");
         }
          else {
-             System.out.println("песня не совпала");
+             System.out.println("song wrong");
         }
         }
     }
